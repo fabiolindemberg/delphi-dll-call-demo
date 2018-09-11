6b4c15c0cdd5cd260d0af2b8cbaac6cea158173b
Fábio Lindemberg <fabiolindemberg>
2018-09-11 13:49:19 -0300

    Adding all project files

---
+++ project/FormDemo.dpr
@@ -0,0 +1,14 @@
+program FormDemo;
+
+uses
+  Forms,
+  main in '..\src\main.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ src/uCustomForm.pas
@@ -0,0 +1,25 @@
+unit uCustomForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TCustomForm = class(TForm)
+    Label1: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TCustomForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
